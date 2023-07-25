c1247a7681ca8c61fe3cfac3cf7ccf926e5be498
Pedro Lucas <izielucas2016@gmail.com>
2023-07-25 11:49:25 -0300

    Atividade Finalizada

--- 02_estrutura_sequencial/atividades/atividade05.js
+++ 02_estrutura_sequencial/atividades/atividade05.js
@@ -4,4 +4,4 @@ let usado = Number(prompt("Quanto tempo o cliente usou o computador em minutos?"
 let valorperMin = valorper15/15
 valorUsado = valorperMin*usado
 
-document.write(`O Cliente tem que pagar ${usado}`)
+document.write(`O Cliente tem que pagar R$${valorUsado}`)
