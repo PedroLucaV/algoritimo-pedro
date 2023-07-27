8d53efb127cb96586025ae534a590ab6319a9e45
Pedro Lucas <izielucas2016@gmail.com>
2023-07-27 08:22:04 -0300

    atualizações

--- 02_estrutura_sequencial/atividades/atividade03.js
+++ 02_estrutura_sequencial/atividades/atividade03.js
@@ -7,4 +7,4 @@ let valorNum = parseFloat(valorKg)
 let valorFinal = consumoNum * (valorNum / 1000)
 let valorFloat = parseFloat(valorFinal)
 
-document.write(`Valor a ser pago: ${valorFloat}`)
+document.write(`Valor a ser pago: ${valorFloat.toFixed(2)}`)
