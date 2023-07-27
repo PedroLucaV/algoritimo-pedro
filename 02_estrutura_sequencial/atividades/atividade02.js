9de93f6b2ffa05d4705d10ae572355ad5dcf289a
Pedro Lucas <izielucas2016@gmail.com>
2023-07-27 07:48:51 -0300

    atividade 2 padronizada

--- 02_estrutura_sequencial/atividades/atividade02.js
+++ 02_estrutura_sequencial/atividades/atividade02.js
@@ -1,8 +1,20 @@
+/*
+Elaborar um programa para uma revenda de
+veículos. O programa deve ler modelo e
+preço do veículo. Apresentar como resposta
+o valor da entrada (50%) e o saldo em 12x.
+*/
+
+//1º Receber o valor das variaveis
 let modelo = prompt("Qual o Modelo do veiculo?")
 let valor = prompt("Qual o valor do carro?")
 
+//2º Casting de Variaveis
 let valorNum = parseFloat(valor)
+
+//2º Processar as variaveis
 let entrada =  valor / 2
 let parcelas = entrada / 12
 
+//3º Mostrar o resultado
 document.write(`Nome do Modelo: ${modelo}, entrada de: R$${entrada}, em 12 parcelas de R$${parcelas}`)
