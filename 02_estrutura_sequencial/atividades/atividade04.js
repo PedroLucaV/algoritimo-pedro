93f021c0a69c54a600f3f716fb81314cc313483a
Pedro Lucas <izielucas2016@gmail.com>
2023-07-27 08:58:36 -0300

    corrigido

--- 02_estrutura_sequencial/atividades/atividade04.js
+++ 02_estrutura_sequencial/atividades/atividade04.js
@@ -3,4 +3,4 @@ let valor = parseFloat(prompt("Agora informe o valor deste medicamento"))
 
 let valorDesc = valor + parseInt(valor)
 
-document.write(`O rmedicamento ${descricao} ficou com um valor de: R$${valor}, caso você compre 2, ele ficará num total de: R$${valorDesc}`)
+document.write(`O medicamento ${descricao} ficou com um valor de: R$${valor}, caso você compre 2, ele ficará num total de: R$${valorDesc}`)
