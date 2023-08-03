1b58dcd8f782fa81f67cd9099ef8da089f52ac09
Pedro Lucas <izielucas2016@gmail.com>
2023-08-03 09:38:07 -0300

    atividade 03 melhorada

--- 07_validacao_de_dados/re_exercicios/ex03.js
+++ 07_validacao_de_dados/re_exercicios/ex03.js
@@ -1,11 +1,9 @@
 let sexo = prompt("Informe seu sexo \n M - Masculino \n F - Feminino:").toUpperCase()
 let altura = parseFloat(prompt("Informe sua altura em metros:"))
+sexo = sexo.charAt(0)
 
 if(altura === 0 || isNaN(altura) || altura >= 2.51){
     alert(`Não foi possivel processar o valor ${altura}`)
-    location.reload()
-}else if(sexo != "M" || sexo != "F"){
-    alert(`Não foi possivel processar o sexo ${sexo}, digite "M" ou "F"`)
     location.reload()
 }
 
@@ -14,7 +12,11 @@ if (sexo == "M"){
     pesoIdeal = 22 * (Math.pow(altura, 2))
 }else if(sexo == "F"){
     pesoIdeal = 21 * (Math.pow(altura, 2))
+}else{
+    alert(`Não foi possivel identificar o sexo ${sexo}`)
+    location.reload()
 }
+
 pesoIdeal = pesoIdeal.toFixed(1)
 
 alert(`Seu peso ideal sendo do sexo biologico: ${sexo} é de: ${pesoIdeal} KG`)
