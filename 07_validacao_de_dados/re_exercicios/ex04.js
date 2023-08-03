01f7761e0272625097a3e2273ac4c67dc46d1891
Pedro Lucas <izielucas2016@gmail.com>
2023-08-03 09:54:06 -0300

    ex04 arrumado

--- 07_validacao_de_dados/re_exercicios/ex04.js
+++ 07_validacao_de_dados/re_exercicios/ex04.js
@@ -1,41 +1,46 @@
 let bairro = prompt("Qual seu bairro?")
 
-bairro = bairro.toUpperCase()
-bairro = bairro.replace(" ", "-")
+if(bairro === ''){
+    alert("Não é possivel ler um espaço vazio")
+    location.reload()
+}else{
+    bairro = bairro.toUpperCase()
+    bairro = bairro.replace(" ", "-")
 
-switch (bairro) {
-    case 'ANTARES':
-        alert(`O Frete para ${bairro} ficou de R$ 7,00`)
-        break;
+    switch (bairro) {
+        case 'ANTARES':
+            alert(`O Frete para ${bairro} ficou de R$ 7,00`)
+            break;
 
-    case 'BARRO-DURO':
-        alert(`O Frete para ${bairro} ficou de R$ 5,00`)
-        break;
+        case 'BARRO-DURO':
+            alert(`O Frete para ${bairro} ficou de R$ 5,00`)
+            break;
 
-    case 'BEBEDOURO':
-        alert(`O Frete para ${bairro} ficou de R$ 8,00`)
-        break;
+        case 'BEBEDOURO':
+            alert(`O Frete para ${bairro} ficou de R$ 8,00`)
+            break;
 
-    case 'BENEDITO-BENTES':
-        alert(`O Frete para ${bairro} ficou de R$ 6,00`)    
-        break;
+        case 'BENEDITO-BENTES':
+            alert(`O Frete para ${bairro} ficou de R$ 6,00`)    
+            break;
 
-    case 'CENTRO':
-        alert(`O Frete para ${bairro} ficou de R$ 10,00`)
-        break;
+        case 'CENTRO':
+            alert(`O Frete para ${bairro} ficou de R$ 10,00`)
+            break;
 
-    case 'CLIMA-BOM':
-        alert(`O Frete para ${bairro} ficou de R$ 4,00`)
-        break;
-    
-    case 'PAJUÇARA':
-        case 'PONTA-VERDE' :
-        case 'JATIUCA' :
-        case 'JATIÚCA':
-        alert(`O Frete para ${bairro} ficou de R$ 15,00`)
-        break;
+        case 'CLIMA-BOM':
+            alert(`O Frete para ${bairro} ficou de R$ 4,00`)
+            break;
+        
+        case 'PAJUÇARA':
+            case 'PONTA-VERDE' :
+            case 'JATIUCA' :
+            case 'JATIÚCA':
+            alert(`O Frete para ${bairro} ficou de R$ 15,00`)
+            break;
 
-    default:
-        alert('Bairro inexistente ou indisponivel!')
-        break;
+        default:
+            alert('Bairro inexistente ou indisponivel!')
+            break;
+    }
 }
