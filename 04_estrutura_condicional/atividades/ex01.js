8699df9a5f18aff2a7b4f1b854a4311b30b3f8a7
Pedro Lucas <izielucas2016@gmail.com>
2023-08-05 21:01:00 -0300

    atividade 01 arrumada com formulario no html

--- 04_estrutura_condicional/atividades/ex01.js
+++ 04_estrutura_condicional/atividades/ex01.js
@@ -1,13 +1,18 @@
-let n1 = Number(prompt("Informe o primeiro número"))
-let n2 = Number(prompt("Informe o segundo número"))
+function comparar(){
+    let tn1 = document.getElementById('caixa1')
+    let tn2 = document.getElementById('caixa2')
+    let res = document.getElementById('res')
 
-if(n1 > n2){
-    alert(`O Maior número é ${n1}`)
-}else if(n1 < n2){
-    alert(`O Maior número é ${n2}`)
-}else if(n1 == n2){
-    alert(`${n1} é igual a ${n2}!`)
-}
-else{
-    alert(`Não foi possivel identificar, por favor, digite um número`)
+    let n1 = Number(tn1.value)
+    let n2 = Number(tn2.value)
+
+    if(n1>n2){
+        res.innerHTML = `O número ${n1} é maior que o número ${n2}!`
+    }else if(n2 > n1){
+        res.innerHTML = `O número ${n2} é maior que o número ${n1}!`
+    }else if(n1 == n2){
+        res.innerHTML = `Os números ${n1} e ${n2} são iguais!`
+    }else{
+        res.innerHTML = "Não foi possivel calcular os valores"
+    }
 }
