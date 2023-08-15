f40bcfc978bd25513625457b07c013324c43249a
Pedro Lucas <izielucas2016@gmail.com>
2023-08-15 10:09:08 -0300

    aas

--- 07_validacao_de_dados/exercicios/ex04.js
+++ 07_validacao_de_dados/exercicios/ex04.js
@@ -1,12 +1,14 @@
-let numero = Number(prompt("Digite um valor:"))
+let numero = Number(prompt("Digite um valor inteiro:"))
 
 if(isNaN(numero)){
-    alert("Digite um número")
+    alert("Digite um valor númerico")
     location.reload()
-}else{
+}else if(Number.isInteger(numero)){
     if(numero % 2 == 0){
         alert(`O Valor ${numero} é Par!`)
     }else{
         alert(`O Valor ${numero} é Impar!`)
     }
+}else{
+    alert("Digite um valor inteiro!!!")
 }
