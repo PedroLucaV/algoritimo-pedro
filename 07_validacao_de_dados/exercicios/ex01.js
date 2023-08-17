e3d02ed8594df40b2e140156d52c0985823b833e
Pedro Lucas <izielucas2016@gmail.com>
2023-08-17 08:47:03 -0300

    atividade 01 corrigida

--- 07_validacao_de_dados/exercicios/ex01.js
+++ 07_validacao_de_dados/exercicios/ex01.js
@@ -1,12 +1,20 @@
-let horaBR = Number(prompt("Me informe a hora e minuto atual: (ex: 1.59 = 1 Hora e 59 Minutos)"))
+let horaBR = Number.parseFloat(prompt("Me informe a hora e minuto no Brasil: (ex: 1.59 = 1 Hora e 59 Minutos)"))
 let horaFR = 0
 
-if (horaBR >= 19){
-    horaFR = (horaBR + 5) - 24
-    horaFR = horaFR.toFixed(2)
-    alert(horaFR+" Horas na França!")
+if(isNaN(horaBR) || horaBR < 0){
+    alert("Informe um valor númerico maior ou igual a 0!!!")
+    location.reload()
+}else if(horaBR > 24){
+    alert("Informe um valor menor que 24!!!")
+    location.reload()
 }else{
-    horaFR = (horaBR + 5)
-    horaFR = horaFR.toFixed(2)
-    alert(horaFR+" Horas na França!")
+    if (horaBR >= 19){
+        horaFR = (horaBR + 5) - 24
+        horaFR = horaFR.toFixed(2)
+        alert(horaFR+" Horas na França!")
+    }else{
+        horaFR = (horaBR + 5)
+        horaFR = horaFR.toFixed(2)
+        alert(horaFR+" Horas na França!")
+    }
 }
--- 07_validacao_de_dados/exercicios/ex06.js
+++ 07_validacao_de_dados/exercicios/ex06.js
@@ -2,13 +2,15 @@ let lado1 = Number(prompt("Digite a medida do primeiro lado"))
 let lado2 = Number(prompt("Digite a medida do segundo lado"))
 let lado3 = Number(prompt("Digite a medida do terceiro lado"))
 let arr = [lado1, lado2, lado3]
+arr = Number(arr)
 
-if(isNaN(arr) || arr <= 0){
+if(isNaN(arr)){
     alert("Não foi possivel processar os números, me informe um valor maior que 0!!!")
     location.reload()
 }else{
     if(lado1 + lado2 < lado3 || lado1 + lado3 < lado2 || lado2 + lado3 < lado1){
         alert("Não foi possivel formar um triangulo")
+        location.reload()
     }else{
         alert("Formou um triangulo!")
         if(lado1 == lado2 && lado1 != lado3 && lado2 != lado3){
@@ -24,3 +26,50 @@ if(isNaN(arr) || arr <= 0){
         }
     }
 }
+
+/*
+let lado1 = Number(prompt("Digite a medida do primeiro lado"))
+if (isNaN(lado1) || lado1 <=0){
+    do{
+        alert("Não foi possivel reconhecer o valor, por favor, digite um Numero maior que 0!!")
+        lado1 = Number(prompt("Digite a medida do primeiro lado"))
+    }while(isNaN(lado1) || lado1 <=0)
+}
+
+let lado2 = Number(prompt("Digite a medida do segundo lado"))
+if (isNaN(lado2) || lado2 <=0){
+    do{
+        alert("Não foi possivel reconhecer o valor, por favor, digite um Numero maior que 0!!")
+        lado2 = Number(prompt("Digite a medida do segundo lado"))
+    }while(isNaN(lado2) || lado2 <=0)
+}
+
+let lado3 = Number(prompt("Digite a medida do terceiro lado"))
+if (isNaN(lado3) || lado3 <=0){
+    do{
+        alert("Não foi possivel reconhecer o valor, por favor, digite um Numero maior que 0!!")
+        lado3 = Number(prompt("Digite a medida do terceiro lado"))
+    }while(isNaN(lado3) || lado3 <=0)
+}
+
+if(lado1 + lado2 < lado3 || lado1 + lado3 < lado2 || lado2 + lado3 < lado1){
+    alert("Não foi possivel formar um triangulo, a página será reiniciada")
+    location.reload()
+}else{
+    alert("Formou um triangulo!")
+    if(lado1 == lado2 && lado1 != lado3 && lado2 != lado3){
+        alert("Formou um triangulo do tipo Isoceles!!!")
+    }else if(lado1 == lado3 && lado1 != lado2 && lado3 != lado2 ){
+        alert("Formou um triangulo do tipo Isoceles!!!")
+    }else if(lado2 == lado3 && lado1 != lado2 && lado3 != lado1){
+        alert("Formou um triangulo do tipo Isoceles!!!")
+    }else if(lado1 == lado2){
+        alert("Formou um triangulo do tipo Equilatero!!!")
+    }else if(lado1 == lado3){
+        alert("Formou um triangulo do tipo Equilatero!!!")
+    }else if(lado3 == lado2){
+        alert("Formou um triangulo do tipo Equilatero!!!")
+    }else{
+        alert("Formou um triangulo do tipo Escaleno!!!")
+    }
+}*/
