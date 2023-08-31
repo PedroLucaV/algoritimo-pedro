e0a13a2d12c859ff2927635e477b1054b52596e1
Pedro Lucas <izielucas2016@gmail.com>
2023-08-31 12:10:19 -0300

    ex03 terminado array

---
+++ 09_vetor(Array)/exercicios/ex03.js
@@ -0,0 +1,17 @@
+let lista = []
+let c = 0
+
+do{
+    lista[c] = prompt("Me informe um item para ser adicionado a lista")
+    if(isNaN(lista[c])){
+        document.write(`${lista[c]} <br>`)
+        c++
+    }else{
+        let sair = confirm("Deseja sair da aplicação?")
+        if(sair == true){
+            break
+        }else{
+            continue
+        }
+    }
+}while(true)
