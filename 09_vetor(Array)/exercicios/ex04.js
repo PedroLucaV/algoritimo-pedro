992a0226e19c4953db73894a1b2b0c2fecee8d08
Pedro Lucas <izielucas2016@gmail.com>
2023-09-05 08:42:38 -0300

    atividade 04 terminada e corrigida

--- 09_vetor(Array)/exercicios/ex04.js
+++ 09_vetor(Array)/exercicios/ex04.js
@@ -1,3 +1,9 @@
+//Intercalar os valores dos vetores 1 e 2 no vetor 3, exemplo
+//Vetor1=[1,3,5]
+//Vetor2=[2,4,6]
+//Vetor3=[1.2.3.4.5.6]
+//Os vetores 1 e 2 tem que ter cada um 10 valores
+
 let arr1 = []
 let arr2 = []
 let arr3 = []
@@ -5,31 +11,25 @@ let arr3 = []
 let c = 0
 let a = 0
 let d = 0
-let lenghtT = 0
+let b = 0
+let e = 0
 
 do{
     arr1[c] = Number(prompt(`Me informe um número para ser alocado a posição ${c}`))
-    arr3[c] = arr1[c]
-    document.write(`${arr1[c]} <br>`)
     c++
 }while(c<10)
 
-document.write(`<hr>`)
-
 do{
     arr2[a] = Number(prompt(`Me informe um número para ser alocado a posição ${a}`))
-    arr3[c] = arr2[a]
-    c++
-    document.write(`${arr2[a]} <br>`)
     a++
 }while(a<10)
-document.write(`<hr>`)
-
-arr3.sort((a,b) => a-b)
-
-lenghtT = arr1.length + arr2.length
 
 do{
+    arr3[d] = arr1[b]
     document.write(`${arr3[d]} <br>`)
     d++
-}while(d<lenghtT)
+    arr3[d] = arr2[b]
+    document.write(`${arr3[d]} <br>`)
+    b++
+    d++
+}while(d<20)
