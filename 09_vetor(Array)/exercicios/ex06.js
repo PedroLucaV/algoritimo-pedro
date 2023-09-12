ae8c03f92a6ddd8aded0789496cd6aea220fca03
Pedro Lucas <izielucas2016@gmail.com>
2023-09-12 09:32:17 -0300

    atividade 08 e 06 terminadas

--- 09_vetor(Array)/exercicios/ex06.js
+++ 09_vetor(Array)/exercicios/ex06.js
@@ -1,16 +1,31 @@
 //Faca um codigo em javascript que organize os valores recebidos em ordem decrescente
 // Crie uma lista para armazenar os valores.
 let valores = []
-let maior = 0
+let aux = 0
 
-for(let i = 0; i<=3; i++){
+//Usando função nativa
+
+    // for(let i = 0; i<10; i++){
+    //     valores.push(Number(prompt("Me informe um número")))
+    // }
+
+    // console.log(valores.sort((a, b) => b-a))
+
+//Usando repetição:
+
+for(let i = 0; i<4; i++){
     valores.push(Number(prompt("Me informe um número")))
-    if(valores > maior){
-        maior = valores[i]
-        valores[i] = maior
-    }else{
-        
+}
+console.log(`Vetor antes da ordenação ${valores}`)
+
+for(let i = 0; i < 4; i++){
+    for(let j = 0; j < 4; j++){
+        if(valores[j] < valores[j++]){
+            aux = valores[j]
+            valores[j] = valores[j++]
+            valores[j++] = aux
+        }
+
     }
 }
-
 console.log(valores)
---
+++ 09_vetor(Array)/exercicios/ex08.js
@@ -0,0 +1,42 @@
+let corretas = []
+let aluno = []
+let resposta
+let notaAl = 0
+let nota = []
+let aprovados = 0
+let porcentagem = 0
+let qntAluno = Number(prompt("Quantos alunos há na sala?"))
+
+for(let i = 0; i < 10; i++){
+    corretas[i] = prompt(`Me informe a alternativa correta da questão ${i+1}; \na, b, c, d ou e`)
+}
+
+for(let i = 0; i < qntAluno; i++){
+    aluno[i] = Number(prompt(`Olá, me informe a matricula do aluno ${i+1}`))
+    for(let j = 0; j < 10; j++){
+        resposta = prompt(`Me informe a resposta do aluno na questão ${j+1}`)
+        if(resposta == corretas[j]){
+            notaAl +=1
+        }else{
+            
+        }
+    }
+    console.log(`Nota do aluno ${i} é igual a = ${notaAl}`)
+    nota[i] = notaAl
+    notaAl = 0
+}
+
+for(let i = 0; i<aluno.length; i++){
+    if(nota[i] >= 6){
+        aprovados+=1
+    }else{
+
+    }
+}
+
+for(let i = 0; i < aluno.length; i++){
+    document.write(`Matricula ${aluno[i]}, Nota: ${nota[i]} <br>`)
+}
+
+porcentagem = (aprovados/aluno.length)*100
+document.write(`<hr>A Media de alunos aprovados é de: ${porcentagem}%`)
